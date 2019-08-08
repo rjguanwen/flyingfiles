c482135c69c4b972dbb8105d7168f3c1a9b0fc58
zhengbin <rjguanwen001@163.com>
2019-08-08 19:53:34 +0800

    优化多协程并行下载模式

--- src/main/file_client.go
+++ src/main/file_client.go
@@ -12,7 +12,6 @@ import (
 	"rjguanwen.cn/flyingfiles/src/myutil"
 	"runtime"
 	"strconv"
-	"sync"
 	"time"
 )
 
@@ -102,7 +101,7 @@ func main() {
 		//splitFilesMD5 := fsi.SplitFilesMD5
 		// 将文件摘要信息写入摘要文件
 		myfileutils.WriteFileConfigYAML(fileName, fsi)
-
+		// ------------------------------ 方法一 ------------------------------
 		// 下面的写法会导致大文件一次创建了太多的协程，生成了太多的链接，超出系统限制的情况时有发生，修改之
 		////为每个子文件创建一个协程
 		//var wg sync.WaitGroup
@@ -112,39 +111,80 @@ func main() {
 		//	go downloadSplitFile(remote, fileName, i, sessionId, &wg) // 下载子文件的协程
 		//}
 		//wg.Wait()
-
-		// 每次最多起10个协程，如果子文件个数超过10个，则按批次进行
-		maxRoutineNum := 10
-		if splitFils <= maxRoutineNum { // 如果子文件数小于最大协程数
-			var wg sync.WaitGroup
-			wg.Add(splitFils)
-			for i := 0; i < splitFils; i++ {
-				// 下载每一个子文件
-				go downloadSplitFile(remote, fileName, i, sessionId, &wg) // 下载子文件的协程
-			}
-			wg.Wait()
-		} else {
-			splitFilesGroups := splitFils / maxRoutineNum
-			if splitFils%maxRoutineNum != 0 {
-				splitFilesGroups += 1
-			}
-			for i := 0; i < splitFilesGroups; i++ { //循环处理每个组
-				var thisLoopNum int // 本次循环需要处理的子文件个数
-				var wg sync.WaitGroup
-				if i == splitFilesGroups-1 {
-					thisLoopNum = splitFils % maxRoutineNum
-				} else {
-					thisLoopNum = maxRoutineNum
-				}
-				wg.Add(thisLoopNum)
-				for j := 0; j < thisLoopNum; j++ {
-					fileSeq := i*maxRoutineNum + j
-					// 下载每一个子文件
-					go downloadSplitFile(remote, fileName, fileSeq, sessionId, &wg) // 下载子文件的协程
-				}
-				wg.Wait() // 通过 Wait，控制分批执行
-			}
+		// ------------------------------ 方法二 ------------------------------
+		// 下面的写法按批次顺序执行，存在某批次中的某个协程挂起或运行缓慢，导致后续批次无法继续执行的风险,修改之
+		//// 每次最多起10个协程，如果子文件个数超过10个，则按批次进行
+		//maxRoutineNum := 10
+		//if splitFils <= maxRoutineNum { // 如果子文件数小于最大协程数
+		//	var wg sync.WaitGroup
+		//	wg.Add(splitFils)
+		//	for i := 0; i < splitFils; i++ {
+		//		// 下载每一个子文件
+		//		go downloadSplitFile(remote, fileName, i, sessionId, &wg) // 下载子文件的协程
+		//	}
+		//	wg.Wait()
+		//} else {
+		//	splitFilesGroups := splitFils / maxRoutineNum
+		//	if splitFils%maxRoutineNum != 0 {
+		//		splitFilesGroups += 1
+		//	}
+		//	for i := 0; i < splitFilesGroups; i++ { //循环处理每个组
+		//		var thisLoopNum int // 本次循环需要处理的子文件个数
+		//		var wg sync.WaitGroup
+		//		if i == splitFilesGroups-1 {
+		//			thisLoopNum = splitFils % maxRoutineNum
+		//		} else {
+		//			thisLoopNum = maxRoutineNum
+		//		}
+		//		wg.Add(thisLoopNum)
+		//		for j := 0; j < thisLoopNum; j++ {
+		//			fileSeq := i*maxRoutineNum + j
+		//			// 下载每一个子文件
+		//			go downloadSplitFile(remote, fileName, fileSeq, sessionId, &wg) // 下载子文件的协程
+		//		}
+		//		wg.Wait() // 通过 Wait，控制分批执行
+		//	}
+		//}
+		// ------------------------------ 方法三 ------------------------------
+		// 采用工作池的方式重写
+		// - 将每个子文件下载看做一个任务，放入任务管道
+		// - 根据指定的最大协程数创建工作协程
+		// - 每个工作协程到循环到任务管道里面获取任务并处理之（下载子文件）
+		// - 每完成一个任务，工作协程将下载结果写入结果信号管道
+		// - 当任务管道中无法获取任务时，工作协程认为工作已经完成并将退出信号写入退出信号管道
+		// - 通过循环取退出信号管道来确定所有工作协程均执行完毕
+		maxRoutineNum := 10            // 同一文件的并发下载协程最大为10
+		if maxRoutineNum > splitFils { // 如果子文件个数小于最大协程数，则修改最大协程数为子文件个数
+			maxRoutineNum = splitFils
 		}
+		taskch := make(chan int, 20)             //任务管道
+		resch := make(chan string, 100)          //结果信号管道
+		exitch := make(chan bool, maxRoutineNum) //退出信号管道
+		// 向任务管道中写入需要下载的子文件编号，每个编号对应一个下载任务
+		go func() {
+			for i := 0; i < splitFils; i++ {
+				taskch <- i
+			}
+			close(taskch)
+		}()
+
+		for i := 0; i < maxRoutineNum; i++ { //启动goroutine做任务
+			go doDownloadTasks(remote, fileName, i, sessionId, taskch, resch, exitch)
+		}
+
+		go func() { //等待goroutine结束
+			for i := 0; i < maxRoutineNum; i++ {
+				<-exitch
+			}
+			close(resch)  //任务处理完成关闭结果管道，不然range报错
+			close(exitch) //关闭退出管道
+		}()
+
+		//打印协程执行信息，本质是通过resch与上面的close(resch)配合来等待作业协程完成工作
+		for res := range resch {
+			mylog.MyTrace.Println("子文件下载协程====>> ", res)
+		}
+		//------------------------------ 方法结束 ------------------------------
 
 		// 合并文件并完成校验
 		isOK, err := myfileutils.MergeSplitFileAndCheck(fileName, fsi)
@@ -172,9 +212,8 @@ func main() {
 
 }
 
-// 请求子数据文件
-func downloadSplitFile(remote string, fileName string, fileSEQ int, sessionId string, wg *sync.WaitGroup) {
-	defer wg.Done()
+// 下载任务
+func doDownloadTasks(remote string, fileName string, fileSEQ int, sessionId string, taskch chan int, resch chan string, exitch chan bool) {
 	defer func() { //异常处理
 		err := recover()
 		if err != nil {
@@ -182,6 +221,23 @@ func downloadSplitFile(remote string, fileName string, fileSEQ int, sessionId st
 			return
 		}
 	}()
+	for i := range taskch { //  处理任务
+		isSuccess := downloadSplitFile(remote, fileName, i, sessionId)
+		var result string
+		if isSuccess {
+			result = "==>" + fileName + ": " + strconv.Itoa(i) + "<== has download."
+		} else {
+			result = "==>" + fileName + ": " + strconv.Itoa(i) + "<== download error!"
+		}
+		resch <- result
+	}
+	exitch <- true //处理完发送退出信号
+}
+
+// 请求子数据文件
+func downloadSplitFile(remote string, fileName string, fileSEQ int, sessionId string) (isSuccess bool) {
+	//func downloadSplitFile(remote string, fileName string, fileSEQ int, sessionId string, wg *sync.WaitGroup) {
+	//defer wg.Done()
 
 	var data = make([]byte, 1024*100) //创建读取服务端信息的切片
 
@@ -232,7 +288,10 @@ func downloadSplitFile(remote string, fileName string, fileSEQ int, sessionId st
 	var isTheEndData = false                                           // 标记收到的数据是不是最后一段数据
 	for {                                                              // 循环接收服务端发送回的数据
 		lengthh, err := con.Read(data) //获取服务器返回信息
-		// 判断接收到的数据是否含有
+		/*
+		 * 此处存在严重bug，择机修改之！！！
+		 */
+		// 判断接收到的数据是否含有overFlag
 		if isSplitFileOver(overFlag, overFlagLength, data, lengthh) {
 			lengthh = lengthh - overFlagLength
 			isTheEndData = true
@@ -280,6 +339,8 @@ func downloadSplitFile(remote string, fileName string, fileSEQ int, sessionId st
 	// 将子文件数据接收完成信息告知服务端
 	con.Write([]byte("OK!"))
 	mylog.MyInfo.Println(fileName + "-" + strconv.Itoa(fileSEQ) + " 数据接收完成！")
+	isSuccess = true
+	return
 }
 
 // 根据接收到的 []byte 判断子文件数据传输是否完成
@@ -287,6 +348,9 @@ func isSplitFileOver(overFlag []byte, overFlagLength int, data []byte, dataLengt
 	if dataLength < overFlagLength {
 		return false
 	} else {
-		return bytes.Equal(overFlag, data[dataLength-overFlagLength:dataLength])
+		// 这个5比较诡异，貌似有时候发送过来的数据会被污染一部分，因此去掉前5个位再比较
+		// 这个办法太烂了，先凑合一下，回头就改掉
+		return bytes.Equal(overFlag[5:], data[dataLength-overFlagLength+5:dataLength])
+		//return string(overFlag) == string(data[dataLength-overFlagLength:dataLength])
 	}
 }
